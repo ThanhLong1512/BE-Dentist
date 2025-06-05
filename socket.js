d2a6e4f0a338568d6431ab4cea9039f88486cf27
TLong_1512 <125524778+ThanhLong1512@users.noreply.github.com>
2025-06-05 23:49:40 +0700

    socket server done

--- socket.js
+++ socket.js
@@ -3,6 +3,38 @@ const io = require("socket.io")(8090, {
     origin: "http://localhost:5173"
   }
 });
+let users = [];
+function addUser(userID, socketID) {
+  !users.some(user => user.userID === userID) &&
+    users.push({ userID, socketID });
+}
+function removeUser(socketID) {
+  users = users.filter(user => user.socketID !== socketID);
+}
+function getUser(userID) {
+  return users.find(user => user.userID === userID);
+}
 io.on("connection", socket => {
   console.log("New client connected");
+  // take UserID and SocketID from client
+  socket.on("addUser", userID => {
+    addUser(userID, socket.id);
+    io.emit("getUsers", users);
+  });
+  //send message and get message from client
+  socket.on("sendMessage", ({ senderID, receiverID, text }) => {
+    const user = getUser(receiverID);
+    if (user) {
+      io.to(user.socketID).emit("getMessage", {
+        senderID,
+        text
+      });
+    }
+  });
+  // when client disconnects
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 });
